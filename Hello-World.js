24fee29cd729bab558cea447c8182e0d67183c4a
jmooliveira <jefferson.mo.oli@gmail.com>
2021-07-03 23:31:51 -0300

    nova branch

--- Hello-World.js
+++ Hello-World.js
@@ -1,9 +1,12 @@
 const helloWorld = () => {
     console.log("Allooo!");
-}
+};
 const aloPp= () => {
     console.log("Oi!");
-}
+};
 const mud = () => {
     console;log("teste para salvar");
-}
+};
+const branch = () => {
+    console;log("nova branch");
+};
